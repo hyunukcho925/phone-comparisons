fc8f2789d20390ce11a4d8bdfd6ce5b2020c1066
hyunukcho925 <hyunukcho@cueing.co.kr>
2025-03-18 10:29:17 +0900

    구글 애널리틱스 tag 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import FontLoader from "./fontloader";
 
@@ -102,6 +103,18 @@ export default function RootLayout({
       <body>
         <FontLoader />
         <div className="container">{children}</div>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-52BFCC30H0"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-52BFCC30H0');
+          `}
+        </Script>
       </body>
     </html>
   );
